fdb8dbd49835ad9e2ec243a5c9a1b98848d8a4f8
Marc Mills <mdmills1990@gmail.com>
2018-09-14 15:05:03 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,8 +5,8 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
 
-binding.pry
 
 create_project_hash
